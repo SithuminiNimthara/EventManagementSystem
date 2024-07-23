4c136cb0c7d1cb87bc9afbe6655dfcb5c76e1949
SithuminiNimthara <158431810+SithuminiNimthara@users.noreply.github.com>
2024-07-23 22:16:24 +0530

    Backend Code

---
+++ src/main/java/com/eventsystem/eventmanagementsystem/exception/EventNotFoundException.java
@@ -0,0 +1,8 @@
+package com.eventsystem.eventmanagementsystem.exception;
+
+public class EventNotFoundException extends RuntimeException{
+    public EventNotFoundException(Long id){
+        super("Could not found the event with id "+id);
+    }
+}
+//comment
