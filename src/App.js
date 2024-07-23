4b344aed1ce11a6340566806c138d9679f77937d
SithuminiNimthara <158431810+SithuminiNimthara@users.noreply.github.com>
2024-07-23 20:19:09 +0530

    frontend code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import EditEvent from "./components/EditEvent";
+import EventList from "./components/EventList";
+import EventForm from "./components/EventForm";
+import EventDetail from "./components/EventDetail";
+import RegistrationForm from "./components/RegistrationForm";
+import Menu from "./layout/Menu";
+import Footer from "./layout/Footer";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import MobNavbar from "./layout/MobNavbar";
 
-function App() {
+function App({ children }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <MobNavbar />
+        {children}
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="menu" element={<Menu />} />
+          <Route path="/addevent" element={<EventForm />} />
+          <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/editevent/:id" element={<EditEvent />} />
+          <Route path="/eventlist" element={<EventList />} />
+          <Route path="/eventdetail/:id" element={<EventDetail />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
